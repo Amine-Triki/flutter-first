b84c945373b214c3f07f8e159186e3ffdf49eace
Amine-Triki <amine.triki15@gmail.com>
2024-09-15 16:29:42 +0100

    c16

--- lib/distination-sreen.dart
+++ lib/distination-sreen.dart
@@ -3,8 +3,33 @@ import 'package:flutter/material.dart';
 class Destination extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              boxShadow: const [
+                BoxShadow(
+                  blurRadius: 20,
+                  offset: Offset(0, 7),
+                  color: Colors.black54,
+                )
+              ],
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image(
+              fit: BoxFit.cover,
+              height: MediaQuery.of(context).size.width,
+              image: const AssetImage('images/dahab2.jpg'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
-
 }
